d73c65bb7f10e375493d6f2e01285630ebf8d4e1
Grace273 <grace131906@gmail.com>
2022-12-02 21:29:37 -0500

    update

--- Fundamentals/JSON-3.py
+++ Fundamentals/JSON-3.py
@@ -10,3 +10,5 @@ print(data[1]['city'])
 # no difference between '' and "" but two of the same set of quote in a row may cause problem
 # var = '"abc"'
 # print(var)
+
+#once you put this list into the file, it saves to your workspace and you don't need to access the website anymore
